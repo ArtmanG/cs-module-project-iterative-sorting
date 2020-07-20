7e33e7e0e232923375f5e1d56d19b541a3c075b7
Gregory Hawman <g.hawman@gmail.com>
2020-07-20 16:42:43 -0700

    finished linear searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,24 @@
+### Linear Search ###
+# return the index of the target if it is in the array.
 def linear_search(arr, target):
-    # Your code here
-
+    if target in arr:
+        return arr.index(target)
 
     return -1   # not found
 
-
+### Binary Search ###
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    high = len(arr)
+    low = 0
+    mid = (high + low) // 2
 
-    # Your code here
+    if arr[mid] == target:
+        return target
+    elif arr[mid] > target:
+        return binary_search(arr, target), mid -1
+    elif arr[mid] < target:
+        return binary_search(arr, target), mid +1
 
-
-    return -1  # not found
+    else:
+        return -1  # not found
